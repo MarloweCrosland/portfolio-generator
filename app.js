6596c945503930f10d6f00f53ba10bc794a8630a
marlowe <marlowepdx@gmail.com>
2022-07-05 23:58:24 -0700

    page template completed

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2);
+const generatePage = require('./src/page-template.js');
+
+const [x, y] = profileDataArgs;
 
 
 
+  fs.writeFile('index.html', generatePage(x,y), err=> {
+      if (err) throw new Error(err);
 
-const printProfileData = x => {
-   x.forEach(profileItem => console.log(profileItem));
-};
+      console.log('portfolio complete Check out index.html');
+  });
