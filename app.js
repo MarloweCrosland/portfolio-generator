9bab25de14e7e9b1f95f52d71b33e13de21eaaab
marlowe <marlowepdx@gmail.com>
2022-07-06 14:16:35 -0700

    page successfully created

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
@@ -130,16 +130,24 @@ if (!portfolioData.projects) {
 
 
 //calling the functions and displaying the answers
-  promptUser()
+promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
-//   fs.writeFile('index.html', pageHTML, err=> {
-//       if (err) throw new Error(err);
-
-//       console.log('portfolio complete Check out index.html');
-//   });
-});
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
 
