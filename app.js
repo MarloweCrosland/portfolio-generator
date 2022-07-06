2d97d2ca49baa42c56d74ff9ce05c6caa126be80
marlowe <marlowepdx@gmail.com>
2022-07-06 12:43:12 -0700

    recursive function for adding more projects

--- app.js
+++ app.js
@@ -1,21 +1,95 @@
 const inquirer = require('inquirer');
-//const fs = require9('fs');
+//const fs = require('fs');
 //const generatePage = require('./src/page-template');
 
-
-inquirer
-.prompt([
+const promptUser = () => {
+return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
         message: 'what is your name?',
-    }
-])
-.then(answers => console.log(answers));
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'what is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself',
+    },
+]);
+};
 
 
 
+const promptProject = (portfolioData) => {
+ // If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
 
+
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+
+      // pushing all the answers from inquirer into projects array
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        //if confirmAdd is true, return and prompt again, if false return answer array
+        if (projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    })
+  };
+
+
+//calling the functions and displaying the answers
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+      console.log(portfolioData);
+});
 
 
 
